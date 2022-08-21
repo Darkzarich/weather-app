cca55c26c7b6a0e5e599cbdff603c49c6ce2cad6
Darkzarich <darkzarichv2@gmail.com>
2022-08-22 00:06:05 +0300

    configure tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "weather-primary": "#5F4B8BFF",
+        "weather-secondary": "#E69A8DFF",
+      }
+    },
+    fontFamily: {
+      Roboto: ["Roboto, sans-serif"],
+    },
+    container: {
+      padding: "2rem",
+      center: true,
+    },
+    screens: {
+      sm: "640px",
+      md: "768px",
+    }
+  },
+  plugins: [],
+}
