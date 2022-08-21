21bdf44504130ef2636532b3c32ac76a2bc3d152
Darkzarich <darkzarichv2@gmail.com>
2022-08-22 00:30:01 +0300

    add navigation

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
+  <div class="flex flex-col min-h-screen font-Roboto bg-weather-primary">
+    <SiteNavigation />
     <RouterView />
   </div>
 </template>
 
 <script setup lang="ts">
+import SiteNavigation from "./components/SiteNavigation.vue";
 
 </script>
