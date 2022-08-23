5a90fb6ba5d5a608bdeeab6c86223312f1d5b073
Darkzarich <darkzarichv2@gmail.com>
2022-08-23 05:51:07 +0300

    handle city search errors

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,17 +8,24 @@
         placeholder="Search for a city or state"
         @input="getSearchResults()"
       />
+
       <ul
-        v-if="searchResults.length"
+        v-if="searchResults"
         class="absolute bg-weather-secondary w-full shadow-md px-1 top-[66px]"
       >
-        <li
-          v-for="result in searchResults"
-          :key="result.id"
-          class="py-2 cursor-pointer"
-        >
-          {{ result.formatted }}
-        </li>
+        <p v-if="searchError">Sorry, something went wrong. Please try again.</p>
+        <p v-else-if="searchResults.length === 0">
+          Not results match your query, try a different one.
+        </p>
+        <template v-else>
+          <li
+            v-for="result in searchResults"
+            :key="result.id"
+            class="py-2 cursor-pointer"
+          >
+            {{ result.formatted }}
+          </li>
+        </template>
       </ul>
     </div>
   </main>
@@ -33,28 +40,39 @@ import { ref } from 'vue';
 
 const searchQuery = ref('');
 
-const searchResults = ref<SearchResult[]>([]);
+const searchResults = ref<SearchResult[] | null>(null);
+
+const searchError = ref(false);
 
 const getSearchResults = debounce(async () => {
-  if (!searchQuery.value) {
+  searchError.value = false;
+
+  if (!searchQuery.value.length) {
+    searchResults.value = null;
+
     return;
   }
 
-  const res = await axios.get<CitiesSearchResponse>(
-    `https://api.geoapify.com/v1/geocode/autocomplete`,
-    {
-      params: {
-        text: searchQuery.value,
-        type: 'city',
-        apiKey: GEOAPIFY_API_KEY,
-      },
-    }
-  );
+  try {
+    const res = await axios.get<CitiesSearchResponse>(
+      `https://api.geoapify.com/v1/geocode/autocomplete`,
+      {
+        params: {
+          text: searchQuery.value,
+          type: 'city',
+          apiKey: GEOAPIFY_API_KEY,
+        },
+      }
+    );
 
-  searchResults.value = res.data.features.map((item) => ({
-    id: item.properties.place_id,
-    name: item.properties.name,
-    formatted: item.properties.formatted,
-  }));
+    searchResults.value = res.data.features.map((item) => ({
+      id: item.properties.place_id,
+      name: item.properties.name,
+      formatted: item.properties.formatted,
+    }));
+  } catch (e) {
+    searchError.value = true;
+    searchResults.value = [];
+  }
 }, 500);
 </script>
