7f66f02232d468d598659436042ff4376fe2ecc7
Darkzarich <darkzarichv2@gmail.com>
2022-09-20 21:03:40 +0300

    change title depending on the page

--- src/router/index.ts
+++ src/router/index.ts
@@ -11,17 +11,28 @@ const routes: Array<RouteRecordRaw> = [
     path: '/',
     name: 'home',
     component: HomeView,
+    meta: {
+      title: 'Home',
+    },
   },
   {
     path: '/weather/:state/:city',
     name: 'city-view',
     component: CityView,
+    meta: {
+      title: 'Weather',
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  document.title = `${to.params.city || to.meta.title} | The Weather App`;
+  next();
 });
 
 export default router;
